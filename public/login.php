38f116696194134d068575992907762bd2c3dd6b
Maxi-Max10 <maxgiu3@gmail.com>
2025-12-16 19:29:00 -0300

    Agregar ícono de la aplicación en las páginas de clientes, dashboard, login, productos y ventas para mejorar la presentación visual.

--- public/login.php
+++ public/login.php
@@ -69,6 +69,7 @@ $csrf = csrf_token();
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title><?= e($appName) ?> - Login</title>
+  <link rel="icon" type="image/png" href="/logo.png">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <style>
